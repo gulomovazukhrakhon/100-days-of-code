b1653b59e452d38a662d3942a1fda80b64c2ed6d
Zukhra <110628098+gulomovazukhrakhon@users.noreply.github.com>
2022-11-07 19:27:35 +0500

    Update main.py

--- mondays-quotes/main.py
+++ mondays-quotes/main.py
@@ -1,1 +1,35 @@
+import datetime as dt
+import random
+import smtplib
+import os
 
+mail = Receiver Mail
+my_password = Sender Password
+my_email = Sender Mail
+
+now = dt.datetime.now()
+week_day = now.weekday()
+quotes = []
+if week_day == 5:
+    with open("quotes.txt") as data_file:
+        data = data_file.readlines()
+        for quote in data:
+            quotes.append(quote)
+
+    random_quote = random.choice(quotes)
+    print(random_quote)
+
+    with smtplib.SMTP_SSL("smtp.mail.yahoo.com", port=465) as connection:
+        # connection.start()
+
+        connection.login(
+            user=my_email,
+            password=my_password
+        )
+
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=mail,
+            msg=f"Subject:Monday Motivational Quote\n\n"
+                f"{random_quote}"
+        )
